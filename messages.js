6d7536300db29fd910b66e5592bd8f7b52f78e91
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-05-29 16:47:25 -0300

    getid e msgs

---
+++ messages.js
@@ -0,0 +1,11 @@
+const MSGS = {
+    'PRODUCT404' : 'Produto não encontrada!',
+    'GENERIC_ERROR' : 'Erro!',
+    'PASSWORD_INVALID' : 'Senha incorreta!',
+    'REQUIRED_PASSWORD' : 'Por favor, insira sua senha',
+    'USER404' : 'Usuário não encontrado!',
+    'VALID_EMAIL' : 'Por favor, insira um email válido',
+    'WITHOUT_TOKEN' : 'Token não enviado', 
+}
+
+module.exports = MSGS
--- routes/api/product.js
+++ routes/api/product.js
@@ -17,92 +17,22 @@ router.get('/', async (req, res) => {
     }
   })
 
-
-//Route GET || Buscar produtos por ID
+  //Route GET || Buscar produtos por ID
 // Acess Public
 router.get('/:id',[], async (req, res) => {
-    try {
-      const id = req.params.id
-      const product = await Product.findOne({_id : id})
-      if(product){
-        res.json(product)
-      } else {
-          res.status(404).send({ "error" : MSGS.PRODUCT404})
-      }
-    } catch (err) {
-      console.error(err.message)
-      res.status(500).send({ "error" : MSGS.GENERIC_ERROR })
+  try {
+    const id = req.params.id
+    const product = await Product.findOne({_id : id})
+    if(product){
+      res.json(product)
+    } else {
+        res.status(404).send({ "error" : MSGS.PRODUCT404})
     }
-  })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send({ "error" : MSGS.GENERIC_ERROR })
+  }
+})
 
-  
-//Route Post || Criar um produto 
-// Acess Public
-router.post('/', [
-    check('id').not().isEmpty(),
-    check('nomeDoProduto').not().isEmpty(),
-    check('fabricante').not().isEmpty(),
-    check('quantidadeDoProduto').not().isEmpty(),
-    check('preco').not().isEmpty(),
-  ], async (req, res, next) => {
-
-    try {
-      let { id, nomeDoProduto, fabricante, quantidadeDoProduto, preco } = req.body
-  
-      const errors = validationResult(req)
-  
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-      } else {
-        let product = new Product({ id, nomeDoProduto, fabricante, quantidadeDoProduto, preco })
-        
-        await product.save()
-  
-        if (product.id) {
-          res.json(product)
-        }
-      }
-    } catch (err) {
-      console.log(err.message)
-      res.status(500).send({ "error": MSGS.GENERIC_ERROR })
-    }
-  })
-
-
-//Route Patch || Alterar produto buscando por ID
-// Acess Public
-  router.patch('/:id', async (req, res, next) => {  
-    try {
-      const id = req.params.id
-      const update = { $set: req.body } // operador $set de update de valor
-      const product = await Product.findByIdAndUpdate(id, update, { new: true })
-      if (product) {
-        res.send(product)
-      } else {
-        res.status(404).send({ error: "Product doesn't exist" })
-      }
-    }catch (err) {
-      console.error(err.message)
-      res.status(500).send({ "error": "Server Error" })
-    }
-  });
-
-
-//Route Delete || Deletar produto por ID
-// Acess Public
-  router.delete('/:id', [], async (req, res, next) => {
-    try {
-      const id = req.params.id
-      const product = await Product.findOneAndDelete({_id: id })
-      if (product) {
-        res.send(product)
-      } else {
-        res.status(404).send({ "error": MSGS.PRODUCT404 })
-      }
-    } catch (err) {
-      console.error(err.message)
-      res.status(500).send({ "error": MSGS.GENERIC_ERROR })
-    }
-  })
 
   module.exports = router
